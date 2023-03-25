10a7070f8c43053e93cb2d6251a0e09f3f8eadb4
Rafi <rafi337161@gmail.com>
2023-03-25 17:36:51 -0500

    saving work - the script doesn't work

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DevOps-Toolkit
 
-Hey there! 👋 Welcome to my DevOps-Toolkit. I'm just starting out with this repo, but my goal is to put together a collection of practical and effective scripts that have helped me streamline and automate things.
+Hey there! 👋 Welcome to my DevOps-Toolkit, a collection of practical and effective scripts that have helped me streamline and automate things.
 ## Categories
 
 - **Deployment**: Tools for automating the deployment process for my projects.
@@ -12,11 +12,11 @@ Hey there! 👋 Welcome to my DevOps-Toolkit. I'm just starting out with this re
 There are usage instructions provided in each `README.md` file.
 ## Installation
 
-To make the `devops-toolkit` scripts easily accessible from your terminal, run the provided `install.sh` script. This script creates symbolic links for all executable scripts in the `/usr/local/bin` directory, allowing you to run the scripts by their name from any location.
+To make the `devops-toolkit` scripts easily accessible from your terminal, run the `install.sh` script. It creates symbolic links for all executable scripts in the `/usr/local/bin` directory, allowing you to run the scripts by their name from any location.
 
 1. Clone the repository: `git clone https://github.com/your-username/devops-toolkit.git`
 2. Change to the `devops-toolkit` directory: `cd devops-toolkit`
-3. Run the `install.sh` script: `./install.sh`
+3. Run the `install.sh` script: `. ./install.sh`
 
 Now you can run any script by its name from your terminal. The `install.sh` script will also ensure that all the scripts are marked as executable.
 
--- automation/remote_connect.sh
+++ automation/remote_connect.sh
@@ -7,34 +7,42 @@ SERVER_USER="${SERVER_USER}"
 SERVER_IP="${SERVER_IP}"
 MAC_ADDRESS="${MAC_ADDRESS}"
 
+remote_check_or_wake() {
+  if ! ping -c 1 -W 1 $SERVER_IP >/dev/null 2>&1; then
+    echo "Sending Wake on LAN signal to server..."
+    ssh "${REMOTE_USER}@${REMOTE_HOST}" "sudo etherwake -i eth0 $MAC_ADDRESS"
+
+    echo "Waiting for server to wake up..."
+    while true; do
+      if ssh -q -o "ConnectTimeout=5" -o "StrictHostKeyChecking=no" "${SERVER_USER}@${SERVER_IP}" "exit" 2>/dev/null; then
+        echo "Server is up!"
+        break
+      else
+        echo "Server is still starting up, retrying in 5 seconds..."
+        sleep 5
+      fi
+    done
+  else
+    echo "Server is already up!"
+  fi
+}
+
 read -p "Would you like to tunnel? [y/N]: " tunnel
 
 if [[ "$tunnel" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    read -p "Specify the port to tunnel (default: 9090): " port
-    port=${port:-9090}
-    echo "Creating SSH tunnel to remote jump host..."
-    ssh -f -N -L $port:localhost:$port "${REMOTE_USER}@${REMOTE_HOST}"
+  read -p "Specify the port to tunnel (default: 9090): " port
+  port=${port:-9090}
+  echo "Creating SSH tunnel to remote jump host..."
+  ssh -f -N -L $port:localhost:$port "${REMOTE_USER}@${REMOTE_HOST}"
 fi
 
-echo "Sending Wake on LAN signal to server..."
-ssh "${REMOTE_USER}@${REMOTE_HOST}" "sudo etherwake -i eth0 $MAC_ADDRESS"
-
-echo "Waiting for server to wake up..."
-while true; do
-  if ssh -q -o "ConnectTimeout=5" -o "StrictHostKeyChecking=no" "${SERVER_USER}@${SERVER_IP}" "exit" 2> /dev/null; then
-    echo "Server is up!"
-    break
-  else
-    echo "Server is still starting up, retrying in 5 seconds..."
-    sleep 5
-  fi
-done
+ssh -t "${REMOTE_USER}@${REMOTE_HOST}" "$(declare -f remote_check_or_wake); remote_check_or_wake"
 
 echo "Connecting to Manjaro Linux server..."
 if [[ "$tunnel" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    ssh -L $port:localhost:$port "${SERVER_USER}@${SERVER_IP}"
+  ssh -L $port:localhost:$port "${SERVER_USER}@${SERVER_IP}"
 else
-    ssh "${SERVER_USER}@${SERVER_IP}"
+  ssh "${SERVER_USER}@${SERVER_IP}"
 fi
 
 # Step 5: Prompt user to shutdown the server
@@ -45,4 +53,3 @@ if [ "$shutdown_choice" == "y" ]; then
 else
   echo "Not shutting down the server. Exiting."
 fi
-
