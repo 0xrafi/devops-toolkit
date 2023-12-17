c1bc51c088e8b1aa72c154ce300dae23391d9e47
Rafi <rafi337161@gmail.com>
2023-12-17 15:45:38 -0600

    fixed remote_connect and tested

--- automation/remote_connect.sh
+++ automation/remote_connect.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#### NOTES, Dec 2023
-# Am learning about 'ProxyJump' SSH config,
-# which removes the need for complex command
-# chaining and remote function execution
-
 # constants
 readonly INITIAL_WAIT_TIME=20
 readonly SUBSEQUENT_WAIT_TIME=5
@@ -17,11 +12,9 @@ jumphost_user="${REMOTE_USER}"
 jumphost="${REMOTE_HOST}" # remote host is duck DNS RPi
 jump_connection="${jumphost_user}@${jumphost}"
 
-manjaro_user="${SERVER_USER}"
 manjaro_ip="${SERVER_IP}" # local IP of Manjaro server
-manjaro_connection="${manjaro_user}@${manjaro_ip}"
-
-mac_address="${MAC_ADDRESS}" # local Mac address of Manjaro server
+manjaro_alias="${MANJARO_SSH_ALIAS}"
+manjaro_mac_address="${MAC_ADDRESS}" # local Mac address of Manjaro server
 
 is_server_online() {
     if ssh -q "${jump_connection}" "ping -c 3 -W 3 $manjaro_ip" >/dev/null 2>&1; then
@@ -42,11 +35,11 @@ is_yes_response() {
 
 wake_up_server() {
   echo "Sending Wake on LAN signal to server..."
-  ssh "${jump_connection}" "sudo etherwake -i eth0 ${mac_address}"
+  ssh "${jump_connection}" "sudo etherwake -i eth0 ${manjaro_mac_address}"
   echo "Waiting for server to wake up..."
   sleep $INITIAL_WAIT_TIME
   while true; do
-    if ssh -q -o "ConnectTimeout=5" -o "StrictHostKeyChecking=no" "${manjaro_connection}" "exit" 2>/dev/null; then
+    if ssh -q -o "ConnectTimeout=5" -o "StrictHostKeyChecking=no" "${manjaro_alias}" "exit" 2>/dev/null; then
       echo "Server is up!"
       break
     else
@@ -58,7 +51,7 @@ wake_up_server() {
 
 initiate_remote_shutdown() {
   echo "Initiating shutdown of the server..."
-  ssh -A -t "${manjaro_connection}" "sudo shutdown now"
+  ssh -A -t "${manjaro_alias}" "sudo shutdown now"
 }
 
 prompt_tunnel_config() {
@@ -89,10 +82,10 @@ check_and_wake_server() {
 establish_manjaro_connection() {
   prompt_tunnel_config
   if is_yes_response "$tunnel"; then
-    ssh -f -N -L $port:localhost:$port "${manjaro_connection}"
+    ssh -f -N -L $port:localhost:$port "${manjaro_alias}"
   else
     echo "Connecting to Manjaro Linux server..."
-    ssh "${manjaro_connection}"
+    ssh "${manjaro_alias}"
   fi
 }
 
